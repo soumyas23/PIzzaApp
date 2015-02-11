a7feec3130f120838a9446d07c6637c8f7473f64
soumyas23 <soumy.s@experionglobal.com>
2015-02-11 12:06:24 +0530

    PizzaApp commited

---
+++ pizzaApp/public/ui/adminPage/adminPage.js
@@ -0,0 +1,21 @@
+angular.module('pizzapp').controller('AdminpageCtrl',function($scope,getuser){
+
+getuser.getpurchases(function(data){
+	$scope.purchases=data;
+	console.log($scope.purchases);
+	
+});
+
+
+$scope.viewPurchase=function(purchase){
+$scope.view=true;
+ $scope.userproducts=purchase.products;
+
+};
+
+$scope.logout=function()
+{
+	getuser.setLogout();
+
+};
+});
